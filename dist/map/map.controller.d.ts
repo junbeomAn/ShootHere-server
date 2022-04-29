0c5c009f08d520e861afd7414c20c77db080c170
junbeoman <wnsqja8744@gmail.com>
2022-04-29 15:57:34 +0900

    remove dist build files

--- dist/map/map.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { MapService } from './map.service';
-export declare class MapController {
-    private readonly mapService;
-    constructor(mapService: MapService);
-    getCoords(query: string): Promise<import("./entities/map.entity").Coords>;
-    getPath(start: string, goal: string): Promise<number[]>;
-    getAddress(lat: string, lng: string): Promise<string>;
-}
